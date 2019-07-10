3044e3061c32dad66bbd30799a4611fcf9d4d862
Votre Nom <Vous@exemple.com>
2019-07-10 10:22:34 +0300

    modification du header

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -78,9 +78,9 @@
                             <div class="col-md-10">
                                 <form class="form-wrap mt-4">
                                     <div class="btn-group" role="group" aria-label="Basic example">
-                                        <input type="text" placeholder="What are your looking for?" class="btn-group1">
-                                        <input type="text" placeholder="New york" class="btn-group2">
-                                        <button type="submit" class="btn-form"><span class="icon-magnifier search-icon"></span>SEARCH<i class="pe-7s-angle-right"></i></button>
+                                        <input type="text" placeholder="Rechercher par devis gratuit" class="btn-group1 header_search">
+                                        <!-- <input type="text" placeholder="New york" class="btn-group2"> -->
+                                        <button type="submit" class="btn-form"><span class="icon-magnifier search-icon"></span>RECHERCHER<i class="pe-7s-angle-right"></i></button>
                                     </div>
                                 </form>
                                 <div class="slider-link">
