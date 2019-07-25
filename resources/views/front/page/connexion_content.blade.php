48ade9e9444775dc4a3c1d1589c75b8bd9e3248f
Njato <randrianasolo.njatofens@gmail.com>
2019-07-25 22:20:26 +0300

    mise en place de la page d'accueil

--- resources/views/front/page/connexion_content.blade.php
+++ resources/views/front/page/connexion_content.blade.php
@@ -57,6 +57,60 @@
         </div>
       </div>
 
+      <div class="item_project padding_new">
+        <label class="new_label">Nouveau</label>
+        <div class="calendar">
+          <img src="{!! url('/image/front/user/calendar.png') !!}" class="" alt="" />
+          <div class="liste_dates">
+            <label for="" class="dates">18</label>
+            <label for="" class="mounth">Jul.</label>
+          </div>
+        </div>
+        <div class="project_desc">
+          <div class="title_item">
+            <label for="" class="title">Chauffage Electrique</label>
+          </div>
+          <div class="city_item">
+            <label for="" class="city">BARENTIN</label>
+            <label for="" class="city_km">- à environ 16 km</label>
+          </div>
+          <div class="description_item">
+            <label for="" class="description_project">Je souhaite réaliser un désembouage pour un radiateurs électrique.</label>
+          </div>
+        </div>
+        <div class="btn_action_item">
+          <button type="button" name="" class="btn_action">Voir le project</button>
+        </div>
+      </div>
+
+      <div class="item_project">
+        <div class="calendar">
+          <img src="{!! url('/image/front/user/calendar.png') !!}" class="" alt="" />
+          <div class="liste_dates">
+            <label for="" class="dates">18</label>
+            <label for="" class="mounth">Jul.</label>
+          </div>
+        </div>
+        <div class="project_desc">
+          <div class="title_item">
+            <label for="" class="title">Chauffage Electrique</label>
+          </div>
+          <div class="city_item">
+            <label for="" class="city">BARENTIN</label>
+            <label for="" class="city_km">- à environ 16 km</label>
+          </div>
+          <div class="description_item">
+            <label for="" class="description_project">Je souhaite réaliser un désembouage pour un radiateurs électrique.</label>
+          </div>
+        </div>
+        <div class="btn_action_item">
+          <button type="button" name="" class="btn_action">Voir le project</button>
+        </div>
+      </div>
+
+      <div class="btn_foot_list">
+        <button type="button" name="" class="btn_view_more_list">Voir tous les projets disponibles</button>
+      </div>
     </div>
   </div>
 </div>
