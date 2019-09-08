e48b09af5b76c949b0e5fc4fbb391bedec4fc121
NjatoFens <njato.njatofens@gmail.com>
2019-09-08 22:02:01 +0300

    mise en page page categories description projecté

--- resources/views/front/categories/categories.blade.php
+++ resources/views/front/categories/categories.blade.php
@@ -7,22 +7,64 @@
 
   @section('content')
 <section class="categories">
+	<div class="title_categories">
+		<h2>Rénover sa maison</h2>
+	</div>
 	<div class="content_category">
+		<div class="item_categories_form">
 			<div class="title_item_cg">
-				<img src="{!! url('/image/categories/bande1.png') !!}" class="img_title_cg" alt="" />
+				<img src="{!! url('/image/front/icones/star.svg') !!}" class="img_title_cg" alt="" />
 				<label class="title_cg">Prix/Description d'un Chauffe eau</label>
 			</div>
 			<div class="item_description">
-				 <div class="desc_row">
+				 <div class="desc_row desc_column">
 				 	<div class="image_desc_row">
 				 		<img src="{!! url('/image/categories/fond2.jpg') !!}" class="img_cg" alt="" />
 				 	</div>
 				 	<div class="content_desc_row">
-				 		<p>Le chauffe eau est un élément capital de nombreuses maisons modernes. En effet à défaut de disposer d’une source d’eau chaude bien à soi, il faut impérativement chauffer son eau de consommation. C’est pourquoi on peut utiliser un système à associer à un chauffage d’eau sanitaire classique (ou à un chauffage électrique).</p>
-				 		<p>Il s’agit bien entendu du chauffe eau qui permet de disposer d’une eau chaude, soit en la chauffant à la volée soit en la chauffant et en la stockant pour une utilisation ultérieure. Nous vous proposons une mise en perspective des différents modèles, pour que vous puissiez calculer le prix d’un chauffe eau selon vos besoins.</p>
+				 		<img src="{!! url('/image/categories/guillemets2.svg') !!}" class="image_cg_commentaire" alt="" />
+				 		<p>Les travaux du bâtiment ne sont pas toujours aisés. Si certains sont faciles d’accès, il est indubitable que d’autres sont très complexes et demandent une planification minutieuse. La rénovation d’une maison, qu’elle soit très ancienne ou simplement vieillotte, implique de recourir à divers travaux qui peuvent s’étaler du très simple au très compliqué.</p>
+				 		<p>Pour réduire les problèmes et les mauvaises surprises, il est donc important de préparer ses travaux à l’avance et de bien comprendre comment chaque étape de votre rénovation de maison peut se dérouler et s’articuler avec la suivante. Nous vous apportons donc un peu d’aide en mettant en lumière des points très précis que la très grande majorité des particuliers rencontrent lors d’une rénovation de maison.	</p>
 				 	</div>
 				 </div>
 			</div>
+		</div>	
+		<div class="item_categories_form">
+			<div class="title_item_cg">
+				<img src="{!! url('/image/front/icones/star.svg') !!}" class="img_title_cg" alt="" />
+				<label class="title_cg">Prix/Description d'un Chauffe eau</label>
+			</div>
+			<div class="item_description">
+				 <div class="desc_row desc_column_reverse">
+				 	<div class="image_desc_row">
+				 		<img src="{!! url('/image/categories/fond2.jpg') !!}" class="img_cg" alt="" />
+				 	</div>
+				 	<div class="content_desc_row">
+				 		<img src="{!! url('/image/categories/guillemets2.svg') !!}" class="image_cg_commentaire" alt="" />
+				 		<p>Les travaux du bâtiment ne sont pas toujours aisés. Si certains sont faciles d’accès, il est indubitable que d’autres sont très complexes et demandent une planification minutieuse. La rénovation d’une maison, qu’elle soit très ancienne ou simplement vieillotte, implique de recourir à divers travaux qui peuvent s’étaler du très simple au très compliqué.</p>
+				 		<p>Pour réduire les problèmes et les mauvaises surprises, il est donc important de préparer ses travaux à l’avance et de bien comprendre comment chaque étape de votre rénovation de maison peut se dérouler et s’articuler avec la suivante. Nous vous apportons donc un peu d’aide en mettant en lumière des points très précis que la très grande majorité des particuliers rencontrent lors d’une rénovation de maison.	</p>
+				 	</div>
+				 </div>
+			</div>
+		</div>	
+		<div class="item_categories_form">
+			<div class="title_item_cg">
+				<img src="{!! url('/image/front/icones/star.svg') !!}" class="img_title_cg" alt="" />
+				<label class="title_cg">Prix/Description d'un Chauffe eau</label>
+			</div>
+			<div class="item_description">
+				 <div class="desc_row desc_column">
+				 	<div class="image_desc_row">
+				 		<img src="{!! url('/image/categories/fond2.jpg') !!}" class="img_cg" alt="" />
+				 	</div>
+				 	<div class="content_desc_row">
+				 		<img src="{!! url('/image/categories/guillemets2.svg') !!}" class="image_cg_commentaire" alt="" />
+				 		<p>Les travaux du bâtiment ne sont pas toujours aisés. Si certains sont faciles d’accès, il est indubitable que d’autres sont très complexes et demandent une planification minutieuse. La rénovation d’une maison, qu’elle soit très ancienne ou simplement vieillotte, implique de recourir à divers travaux qui peuvent s’étaler du très simple au très compliqué.</p>
+				 		<p>Pour réduire les problèmes et les mauvaises surprises, il est donc important de préparer ses travaux à l’avance et de bien comprendre comment chaque étape de votre rénovation de maison peut se dérouler et s’articuler avec la suivante. Nous vous apportons donc un peu d’aide en mettant en lumière des points très précis que la très grande majorité des particuliers rencontrent lors d’une rénovation de maison.	</p>
+				 	</div>
+				 </div>
+			</div>
+		</div>	
 	</div>
 </section>
  @endsection
