53cf43738707327ab320434771d11aeea4912661
Misaina <misainarakotomanolo@yahoo.fr>
2019-11-05 14:13:33 +0300

    stripe test reussite

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -4,8 +4,10 @@
 namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
-use Stripe\Stripe;
-use Stripe\Charge;
+//use Stripe\Stripe;
+//use Stripe\Charge;
+use Session;
+use Stripe;
  
 class StripeController extends Controller
 {
@@ -29,42 +31,54 @@ public function stripe()
  */
 public function payStripe(Request $request)
 {
-    $this->validate($request, [
-        'card_no' => 'required',
-        'expiry_month' => 'required',
-        'expiry_year' => 'required',
-        'cvv' => 'required',
-    ]);
+     Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe\Charge::create ([
+                "amount" => 100 * 100,
+                "currency" => "usd",
+                "source" => $request->stripeToken,
+                "description" => "Test payment from itsolutionstuff.com." 
+        ]);
+  
+        Session::flash('success', 'Payment successful!');
+          
+        return back();
+
+//     $this->validate($request, [
+//         'card_no' => 'required',
+//         'expiry_month' => 'required',
+//         'expiry_year' => 'required',
+//         'cvv' => 'required',
+//     ]);
  
-$stripe = Stripe::make('sk_test_pOmOcaYxfBK9EEcVzw6bh9XG');
-try {
-    $token = $stripe->tokens()->create([
-        'card' => [
-            'number'    => $request->get('card_no'),
-            'exp_month' => $request->get('expiry_month'),
-            'exp_year'  => $request->get('expiry_year'),
-            'cvc'       => $request->get('cvv'),
-        ],
-    ]);
-    if (!isset($token['id'])) {
-        return Redirect::to('strips')->with('Token is not generate correct');
-    }
-    $charge = $stripe->charges()->create([
-        'card' => $token['id'],
-        'currency' => 'USD',
-        'amount'   => 20,
-        'description' => 'Register Event',
-    ]);
-    $charge = Charge::create(array(
-        'amount' => 20,
-        "source" => $token,
-        'currency' => 'usd'
-    ));
+// $stripe = Stripe::make('sk_test_pOmOcaYxfBK9EEcVzw6bh9XG');
+// try {
+//     $token = $stripe->tokens()->create([
+//         'card' => [
+//             'number'    => $request->get('card_no'),
+//             'exp_month' => $request->get('expiry_month'),
+//             'exp_year'  => $request->get('expiry_year'),
+//             'cvc'       => $request->get('cvv'),
+//         ],
+//     ]);
+//     if (!isset($token['id'])) {
+//         return Redirect::to('strips')->with('Token is not generate correct');
+//     }
+//     $charge = $stripe->charges()->create([
+//         'card' => $token['id'],
+//         'currency' => 'USD',
+//         'amount'   => 20,
+//         'description' => 'Register Event',
+//     ]);
+//     $charge = Charge::create(array(
+//         'amount' => 20,
+//         "source" => $token,
+//         'currency' => 'usd'
+//     ));
  
-     return 'Payment Success';
-    } catch (\Exception $ex) {
-        return $ex->getMessage();
-    }
+//      return 'Payment Success';
+//     } catch (\Exception $ex) {
+//         return $ex->getMessage();
+//     }
         
-  }  
+   }  
 }
