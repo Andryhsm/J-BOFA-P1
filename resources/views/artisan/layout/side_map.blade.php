1dbf76fc6a5cc0278e60e2f8fd635617d70b4f7d
Misaina <misainarakotomanolo@yahoo.fr>
2019-11-08 11:25:53 +0300

    integration map

--- resources/views/artisan/layout/side_map.blade.php
+++ resources/views/artisan/layout/side_map.blade.php
@@ -11,15 +11,15 @@
       <div id="map_canvas" class="mapping"></div>
   </div>
 
-    <!-- <div class="maps_location"> -->
+    <div class="maps_location">
       <!-- <img src="{!! url('/image/artisan/maps.png') !!}" class="" alt="maps" /> -->
       <!-- <div id="mymap"></div> -->
-      <!-- <iframe width='100%' height='100%' id='mapcanvas' src='https://maps.google.com/maps?q=London,%20United%20Kingdom&Roadmap&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class='zxos8_gm'><a href='https://www.hercampus.com/author/jessica-cliembielle'>Google</a></div><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:700px;'></div><div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small></div></iframe>
+      <iframe width='100%' height='100%' id='mapcanvas' src='https://maps.google.com/maps?q=London,%20United%20Kingdom&Roadmap&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div class='zxos8_gm'><a href='https://www.hercampus.com/author/jessica-cliembielle'>Google</a></div><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:700px;'></div><div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small></div></iframe>
 
       <iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=k&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe>
     </div>
     <div class="btn_foot_list">
       <button type="button" name="" class="btn_view_more_list">Modifier ma zone de prospection</button>
     </div>
-  </div> -->
+  </div>
 </div>
