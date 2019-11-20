60fb6df50be6392406051b124817346c10f1bac6
Votre Nom <Vous@exemple.com>
2019-11-20 16:04:19 +0300

    modification de footer

--- resources/views/front/layout/footer.blade.php
+++ resources/views/front/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <!--============================= FOOTER =============================-->
-  <footer class="main-block dark-bg">
+  <footer class="main-block dark-bg relative_footer">
       <div class="container">
           <div class="row">
               <div class="col-md-12">
@@ -13,6 +13,9 @@
                   </div>
               </div>
           </div>
+      </div>
+      <div class="absolute_footer">
+        <a href="{{ url('/admin') }}" class="administration_link">Administration</a>
       </div>
   </footer>
   <!--//END FOOTER -->
