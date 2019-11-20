0676f35efdbf2fec34a2f7e004b2d3df3af00a9d
Misaina <misainarakotomanolo@yahoo.fr>
2019-11-20 15:22:19 +0300

    pro artisan

--- app/Http/Controllers/Artisan/ArtisanController.php
+++ app/Http/Controllers/Artisan/ArtisanController.php
@@ -179,7 +179,7 @@ class ArtisanController extends Controller
             $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
             $hours = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24) / (60*60));
             $minutes = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60)/ 60);
-            return $minutes;
+            return $hours;
         }else{
             $hours=0;
             return $hours;
--- resources/views/artisan/layout/menu.blade.php
+++ resources/views/artisan/layout/menu.blade.php
@@ -4,20 +4,20 @@
       <label for="">Accueil</label>
     </div>
   </a>
-  <a class="menus" onclick="{{($diff >0 ) ? 'return true' : 'return false'}}">
+  <a class="menus" data-val="{{$diff}}" onclick="{{($diff  <=0 || $diff>1 ) ? 'return false' : 'return true'}}">
     <div class="item_menu" >
       <label for="">Projet Disponible</label>
       <label for="" class="notification">8</label>
       <label for="" class="pro_version">Pro</label>
     </div>
   </a>
-  <a class="menus" onclick="{{($diff >0 ) ? 'return true' : 'return false'}}">
+  <a class="menus" data-val="{{$diff}}" onclick="{{($diff  <=0 || $diff>1 ) ? 'return false' : 'return true'}}">
     <div class="item_menu">
       <label for="">Mes projet accéptés</label>
       <label for="" class="pro_version">Pro</label>
     </div>
   </a>
-  <a class="menus" onclick="{{($diff >0 ) ? 'return true' : 'return false'}}">
+  <a class="menus" data-val="{{$diff}}" onclick="{{($diff  <=0 || $diff>1 ) ? 'return false' : 'return true'}}">
     <div class="item_menu">
       <label for="">Mes évaluations</label>
       <label for="" class="pro_version">Pro</label>
@@ -28,13 +28,13 @@
       <label for="">Mon profil</label>
     </div>
   </a>
-  <a class="menus" onclick="{{($diff >0 ) ? 'return true' : 'return false'}}">
+  <a class="menus" data-val="{{$diff}}" onclick="{{($diff <=0 || $diff>1 ) ? 'return false' : 'return true'}}">
     <div class="item_menu">
       <label for="">Mes factures</label>
       <label for="" class="pro_version">Pro</label>
     </div>
   </a>
-  <a class="menus" onclick="{{($diff >0 ) ? 'return true' : 'return false'}}" href="{{ route('service') }}">
+  <a class="menus" data-val="{{$diff}}" onclick="{{($diff <=0 || $diff>1 ) ? 'return false' : 'return true'}}" href="{{ route('service') }}">
     <div class="item_menu">
       <label for="">Services</label>
       <label for="" class="add_service">+</label>
