95a5dc393971f628b771f502b7f7fe45a706b558
Lydia Andrianjatovo <lydiaandrianjatovo@gmail.com>
2019-11-30 16:05:08 +0300

    Amélioration projet accepté

--- resources/views/artisan/page/project_accepted.blade.php
+++ resources/views/artisan/page/project_accepted.blade.php
@@ -1,8 +1,10 @@
 @extends('artisan.layout.master')
 
     @section('additional-css')
+    {!! Html::style('css/admin/home.css') !!}
+    {!! Html::style('css/admin/main.css') !!}
       {!! Html::style('css/front/project_available.css') !!}
-      {!! Html::style('css/admin/home.css') !!}
+      {!! Html::style('css/admin/project_accepted.css') !!}
     @endsection
 
     @section('banner')
@@ -23,13 +25,13 @@
         <div class="advisor-card">
           <div class="row">
               <div class="col-12">
-                  <h2>
-                                          Mon conseiller
-                                  </h2>
-                  <a href="https://membre.habitatpresto.com/rappel" title="Être appelé" class="be-called fancybox-ajax">
-                      Être appelé<span class="arrow-link"></span>
-                  </a>
-                  <div class="clearfix"></div>
+                <div class="d-flex justify-content-between">
+                    <h2>Mon conseiller</h2>
+                    <a href="https://membre.habitatpresto.com/rappel" title="Être appelé" class="be-called fancybox-ajax d-flex align-items-center">
+                        Être appelé<i class="fa fa-angle-right"></i>
+                    </a>
+                </div>
+                  
                   <div class="contact text-center">
                                           <p class="name">Maeva Leroy</p>
                                       <p class="number">09 74 73 80 87</p>
@@ -48,77 +50,81 @@
     @endsection
 
     @section('content')
-      <h1>Si projet vide</h1>
-      <div class="pages_artisan project_list_page">
-        <div class="last-accepted-project">
-            <h2 class="title">Mes projets acceptés</h2>
-            <p class="mt-4">
-                Vous n'avez pas encore accepté de projets.
-            </p>
-            <p class="mt-4 lh-20">
-                Dès que vous accepterez un projet, il sera affiché ici.
-                Vous pourrez suivre l'avancée de vos projets en mettant à jour leur statut (particulier à contacter, rendez-vous pris, devis à faire...).
-            </p>
-        </div>
-        <div class="completed-projects-block">
-            <h2 class="title">Mes projets terminés avec succès</h2>
-            <p class="mt-4">
-                Vous n'avez pas encore finalisé de projets.
-            </p>
-            <p class="mt-4">
-                Dès que vous avez terminé l'un de vos projets, n'oubliez pas de mettre à jour son statut afin d'en faciliter le suivi.
-            </p>
-        </div>
-      </div>
+    
+      @if(count($project_availables) > 0)  
+        <div class="pages_artisan project_list_page">
+          <div class="liste_project flex_one">
+            <div class="project_item {{(Auth::user()->email_verified_at == null) ? 'mail_not_active' : ''}}">
+              <div class="title_project">
+                <label for="" class="number_project">{{isset($project_availables) ? count($project_availables) : 0}}</label>
+                <label for="" class="title_project_item">PROJETS ACCEPTE</label>
+              </div>
+              @if(isset($project_availables))
+              @foreach($project_availables as $project)
+              <div class="item_project d-flex flex-wrap">
+                <div class="calendar">
+                  <img src="{!! url('/image/front/user/calendar.png') !!}" class="" alt="" />
+                  <div class="liste_dates">
+                    <label for="" class="dates">18</label>
+                    <label for="" class="mounth">Jul.</label>
+                  </div>
+                </div>
+                <div class="project_desc flex_one">
+                  <div class="title_item">
+                    <label for="" class="title">{{$project->project->category->name}}</label>
+                  </div>
+                  <div class="city_item">
+                    <label for="" class="city">{{$project->project->city->ville_nom}}</label>
+                    <label for="" class="city_km">- à environ 16 km</label>
+                  </div>
+                  <div class="description_item">
+                    <label for="" class="description_project">Je souhaite réaliser un désembouage pour un radiateurs électrique.</label>
+                  </div>
+                </div>
+                <div class="btn_action_item">
+                  <a href="{{ url('/artisan/project/'.$project->project_id.'') }}"  name="" class="btn_view_project_detail d-flex justify-content-center align-items-center">
+                    <span>Voir le project</span>
+                  </a>
+                </div>
+                <div class="border_bottom border_dashed"></div>
+              </div>
+              @endforeach
+              @endif
 
-      <h1>Si non vide</h1>
-      <div class="pages_artisan project_list_page">
-        <div class="liste_project flex_one">
-          <div class="project_item {{(Auth::user()->email_verified_at == null) ? 'mail_not_active' : ''}}">
-            <div class="title_project">
-              <label for="" class="number_project">{{isset($project_availables) ? count($project_availables) : 0}}</label>
-              <label for="" class="title_project_item">PROJETS ACCEPTE</label>
             </div>
-            @if(isset($project_availables))
-            @foreach($project_availables as $project)
-            <div class="item_project d-flex flex-wrap">
-              <div class="calendar">
-                <img src="{!! url('/image/front/user/calendar.png') !!}" class="" alt="" />
-                <div class="liste_dates">
-                  <label for="" class="dates">18</label>
-                  <label for="" class="mounth">Jul.</label>
-                </div>
-              </div>
-              <div class="project_desc flex_one">
-                <div class="title_item">
-                  <label for="" class="title">{{$project->project->category->name}}</label>
-                </div>
-                <div class="city_item">
-                  <label for="" class="city">{{$project->project->city->ville_nom}}</label>
-                  <label for="" class="city_km">- à environ 16 km</label>
-                </div>
-                <div class="description_item">
-                  <label for="" class="description_project">Je souhaite réaliser un désembouage pour un radiateurs électrique.</label>
-                </div>
-              </div>
-              <div class="btn_action_item">
-                <a href="{{ url('/artisan/project/'.$project->project_id.'') }}"  name="" class="btn_view_project_detail d-flex justify-content-center align-items-center">
-                  <span>Voir le project</span>
-                </a>
-              </div>
-              <div class="border_bottom border_dashed"></div>
+
+            <div class="mail_confirm {{(Auth::user()->email_verified_at != null) ? 'mail_not_active' : ''}}" >
+                <label for="" class="activate_mail mail_thanks">Merci Beaucoup pour votre inscription!</label>
+                <label for="" class="activate_mail">SVP, Veillez cliquer sur le lien de confirmation de votre compte dans votre boite mail</label>
             </div>
-            @endforeach
-            @endif
-
-          </div>
-
-          <div class="mail_confirm {{(Auth::user()->email_verified_at != null) ? 'mail_not_active' : ''}}" >
-              <label for="" class="activate_mail mail_thanks">Merci Beaucoup pour votre inscription!</label>
-              <label for="" class="activate_mail">SVP, Veillez cliquer sur le lien de confirmation de votre compte dans votre boite mail</label>
           </div>
         </div>
-      </div>
+        
+      @else
+
+        <div class="pages_artisan project_list_page">
+          <div class="last-accepted-project basic-background">
+              <h2 class="title">Mes projets acceptés</h2>
+              <p class="mt-4">
+                  Vous n'avez pas encore accepté de projets.
+              </p>
+              <p class="mt-4 lh-20">
+                  Dès que vous accepterez un projet, il sera affiché ici.
+                  Vous pourrez suivre l'avancée de vos projets en mettant à jour leur statut (particulier à contacter, rendez-vous pris, devis à faire...).
+              </p>
+          </div>
+          <div class="completed-projects-block basic-background">
+              <h2 class="title">Mes projets terminés avec succès</h2>
+              <p class="mt-4">
+                  Vous n'avez pas encore finalisé de projets.
+              </p>
+              <p class="mt-4">
+                  Dès que vous avez terminé l'un de vos projets, n'oubliez pas de mettre à jour son statut afin d'en faciliter le suivi.
+              </p>
+          </div>
+        </div>
+      @endif
+
     @endsection
 
     @section('additional-script')
