9359c9b307e1e9c26d9c70cdce032f698ffeaf47
Misaina <misainarakotomanolo@yahoo.fr>
2019-12-06 10:44:36 +0300

    Change menu

--- resources/views/artisan/layout/menu.blade.php
+++ resources/views/artisan/layout/menu.blade.php
@@ -8,48 +8,48 @@
     <div class="item_menu" >
       <label for="">Projet Disponible</label>
       <!-- <label for="" class="notification">8</label> -->
-      @if($diff  <=0 || $diff>3)
+      <!-- @if($diff  <=0 || $diff>3)
         <label for="" class="pro_version">Pro</label>
-      @endif
+      @endif -->
     </div>
   </a>
 
   <a class="menus" href="{{route('project_accepted')}}" data-val="{{$diff}}">
     <div class="item_menu">
       <label for="">Mes projet accéptés</label>
-      @if($diff  <=0 || $diff>3)
+      <!-- @if($diff  <=0 || $diff>3)
         <label for="" class="pro_version">Pro</label>
-      @endif
+      @endif -->
     </div>
   </a>
-  <a class="menus" data-val="{{$diff}}" onclick="{{($diff  <=0 || $diff>3 ) ? 'return false' : 'return true'}}">
+  <!-- <a class="menus" data-val="{{$diff}}" onclick="{{($diff  <=0 || $diff>3 ) ? 'return false' : 'return true'}}">
     <div class="item_menu">
       <label for="">Mes évaluations</label>
       @if($diff  <=0 || $diff>3)
         <label for="" class="pro_version">Pro</label>
       @endif
     </div>
-  </a>
+  </a> -->
   <a class="menus" href="{{ Url('/artisan/profil/'.Auth::user()->id.'') }}">
     <div class="item_menu">
       <label for="">Mon profil</label>
     </div>
   </a>
-  <a class="menus" data-val="{{$diff}}" onclick="{{($diff <=0 || $diff>3 ) ? 'return false' : 'return true'}}">
+  <a class="menus" data-val="{{$diff}}">
     <div class="item_menu">
       <label for="">Mes factures</label>
-      @if($diff  <=0 || $diff>3)
+      <!-- @if($diff  <=0 || $diff>3)
         <label for="" class="pro_version">Pro</label>
-      @endif
+      @endif -->
     </div>
   </a>
   <a class="menus" data-val="{{$diff}}" onclick="{{($diff <=0 || $diff>3 ) ? 'return false' : 'return true'}}" href="{{ route('service') }}">
     <div class="item_menu">
       <label for="">Services</label>
       <label for="" class="add_service">+</label>
-      @if($diff  <=0 || $diff>3)
+      <!-- @if($diff  <=0 || $diff>3)
         <label for="" class="pro_version">Pro</label>
-      @endif
+      @endif -->
     </div>
   </a>
 </div>
