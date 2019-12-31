1d098f0af5d151425d94c63f729f6ba56536bd1e
Benja <andryhsm@gmail.com>
2019-12-31 11:01:39 +0300

    cgv

--- resources/views/artisan/page/payment.blade.php
+++ resources/views/artisan/page/payment.blade.php
@@ -23,6 +23,129 @@
             width: 61%;
         }
     </style>
+    <style type="text/css">
+        .artisan-body {
+            margin: 0;
+        }
+
+        .artisan-email-page {
+            display: -webkit-box;display: -ms-flexbox;display: flex;
+            -webkit-box-pack: center;-ms-flex-pack: center;justify-content: center;
+            -webkit-box-align: center;-ms-flex-align: center;align-items: center;
+            -ms-flex-wrap: wrap;flex-wrap: wrap;
+            max-width: 800px;
+            margin: 40px   auto;
+            background: #ffffff;
+            padding: 0;
+        }
+
+        .artisan-email-page * {
+            font-family: Roboto;
+            box-sizing: border-box;
+        }
+
+        .artisan-page-header {
+            width: 100%;
+            margin: 0 5px;
+        }
+
+        .artisan-page-content {
+            width: 100%;
+            margin: 0 15px;
+        }
+
+        .artisan-email-page p {
+            margin: 0;
+        }
+
+        .artisan-date {
+            width: 100%;
+            text-align: center;
+            color: #868686;
+            font-size: 14px;
+            margin-bottom: 15px!important;
+        }
+
+        .artisan-mail-title {
+            width: 100%;
+            max-width: 100%;
+            padding: 20px;
+            background: #23abdf;
+            margin-bottom: 30px;
+        }
+
+        .artisan-mail-title p {
+            width: 100%;
+            text-align: center;
+            color: #ffffff;
+            font-size: 18px;
+        }
+
+        .artisan-mail-title p:not(:last-child) {
+            margin-bottom: 8px;
+        }
+
+        .artisan-introduction-text {
+
+        }
+
+        .artisan-introduction-text p.artisan-hello {
+            margin-bottom: 25px;
+        }
+
+        .artisan-introduction-text p {
+            color: #818181;
+            font-size: 15px;
+            margin-bottom: 40px;
+            line-height: 1.3;
+        }
+
+        .artisan-firma {
+            width: 100%;
+            margin-bottom: 45px;
+        }
+
+        .artisan-information {
+            border: 2px solid #6e9bae;
+            padding: 8px;
+        }
+
+        .artisan-information-wrapper {
+            display: -webkit-box;display: -ms-flexbox;display: flex;
+        }
+
+        .artisan-information-wrapper:not(:last-child) {
+            margin-bottom: 8px;
+        }
+
+        .artisan-information label {
+            color: #407da8;
+            font-weight: bold;
+            width: 120px;
+            text-align: right;
+            margin-right: 8px;
+        }
+
+        .artisan-information span {
+            color: #676767;
+            font-size: 15px;
+            font-weight: 400;
+            text-transform: uppercase;
+            -webkit-box-flex: 1;-ms-flex: 1;flex: 1;
+        }
+
+        .artisan-firma p {
+            color: #787878;
+            margin-bottom: 15px;
+        }
+
+        .artisan-mail-footer p {
+            color: #787878;
+            font-size: 15px;
+            margin-bottom: 15px;
+        }
+
+    </style>
     @endsection
 
     @section('banner')
--- resources/views/artisan/page/stripePayment.blade.php
+++ resources/views/artisan/page/stripePayment.blade.php
@@ -74,7 +74,90 @@
                             </div>
                             <div class='form-row row' id="cgv_txt">
                                 <div class='col-md-12  form-group hide' id="error">
-                                    <div class='alert-warning alert'>Condition de vente </div>
+                                    <div class='alert-warning alert'>
+                                        <div class="artisan-email-page">
+                                            <div class="artisan-page-header">
+
+                                                <div class="artisan-mail-title">
+                                                    <p>CONDITION DE GESTION GENERALE DE VENTE</p>
+                                                </div>
+                                            </div>
+                                            <div class="artisan-page-content">
+                                                <div class="artisan-introduction-text">
+                                                    <p class="artisan-hello"><h1>INTRODUCTION</h1><p>
+                                                    <p>Le Site Internet accessible à l'adresse URL www.e-artisans.fr (ci-après « <span style="font-weight: bold; ">le Site<br>
+                                                        E-artisans. Fr/connexion </span>») est la propriété de l’éditeur en programmation informatique Jaouad Azoum entrepreneur
+                                                        individuel<br><br>
+                                                        Inscrit au Registre du Commerce de Rouen SIRÈN 798351763 ,SIRET 79835176300023 , dont le siège social est
+                                                        situé au 84 route de bonsecours 76000 Rouen
+                                                        <p>Par le biais de ce Site, E-artisans. Fr entend faciliter la rencontre entre des personnes en recherche de professionnels
+                                                        pour réaliser diverses prestations de travaux (ci-après « <span style="font-weight: bold; ">les Utilisateurs</span> ») et lesdits professionnels, membres du
+                                                        réseau e-artisans.frinscrits sur le Site e-artisans.frPRO (ci-après « <span style="font-weight: bold; ">les Membres</span>»). Dans ce cadre, HABITATPRESTO
+                                                        met à disposition des Utilisateurs un service leur permettant d'effectuer en ligne une demande de devis, à laquelle
+                                                        plusieurs Membres pourront répondre.</p>
+                                                        <p>Les Membres accèdent aux services de E-artisans.fr par le paiement d'une cotisation mensuelle.
+                                                        Les Utilisateurs peuvent effectuer leurs demandes de devis par le biais du site Internet accessible à l'adresse URl<br>
+                                                        <span style="color: #ff580c;">https://www.e-artisans.fr</span></p>
+                                                        <p>Une fois la prise de contact effectuée entre l'Utilisateur et le Membre, l'éventuelle poursuite de la relation est effectuée
+                                                        en-dehors des Sites e-arisans.fr de Jaouad AZOUM n'intervient en aucun cas dans la formation d'un éventuel accord
+                                                        entre ces deux parties.
+
+                                                    </p>
+                                                    <p>
+                                                        <h1>ARTICLE 1 - DÉFINITIONS</h1>
+                                                    </p>
+                                                    <p>Les termes suivants désignent :
+
+                                                        « <span style="font-weight: bold; ">Abonnement </span>» : Désigne le droit d'accès au Service pour le Membre, ayant pour contrepartie le versement d'une
+                                                        cotisation mensuelle.
+                                                    </p>
+                                                    <p>
+                                                        « <span style="font-weight: bold; ">Bon de commande</span> » : Désigne le document envoyé par email au Membre récapitulant le ou les Services souscrits.
+                                                        « <span style="font-weight: bold; ">Demande de devis</span> » : Désigne le Processus permettant à l'Utilisateur, recherchant un professionnel pour exécuter
+                                                        une prestation de service déterminée, de détailler cette recherche afin de recevoir par la suite des devis de
+                                                        professionnels Membres de E-artisans.fr
+                                                    </p>
+                                                    <p>
+                                                        « <span style="font-weight: bold; ">Données ou Contenu(s)</span> » : Désigne les informations de toute nature, sous forme numérique, comme par exemple
+                                                        un texte, une image, une vidéo, un fichier etc.
+                                                    </p>
+                                                    <p>
+                                                        « <span style="font-weight: bold; ">Espace personnel </span>» : Désigne l'espace, sur le Site e-artisans.fr , auquel l'Utilisateur peut se connecter par
+                                                        l'utilisation d'un identifiant et d'un mot de passe et contenant des informations personnelles et lui permettant
+                                                        d'utiliser l'Outil.
+                                                    </p>
+                                                    <p>
+                                                        « <span style="font-weight: bold; ">Fiche Membre </span>» : Désigne la page du Membre sur laquelle sont indiquées toutes ses informations, et tous les avis
+                                                        laissés par les Utilisateurs à son sujet. Cette Fiche Membre est publique.
+                                                    </p>
+                                                </div>
+
+                                                <!-- <div class="artisan-firma">
+                                                    <p>Avec la société<p>
+                                                    <div class="artisan-information">
+                                                        <div class="artisan-information-wrapper">
+                                                            <label>Société&nbsp;:</label><span>inspection et rénovation de l'habitat</span>
+                                                        </div>
+                                                        <div class="artisan-information-wrapper">
+                                                            <label>Adresse&nbsp;:</label><span>7 rue marconi</span>
+                                                        </div>
+                                                        <div class="artisan-information-wrapper">
+                                                            <label>Téléphone&nbsp;:</label><span>066215444</span>
+                                                        </div>
+                                                    </div>
+                                                </div> -->
+
+                                                <div class="artisan-mail-footer">
+                                                    <p>Dès à présent vous pouvez contacter cette entreprise pour fixer un rendez-vous. Selon notre charte de qualité, les professionnels s'engagent à prendre contact avec vous sous 72h.</p>
+
+                                                    <p>Nous vous remercions d'avoir fait appel au service HelloArtisan. Pour toute information complémentaire n'hésitez pas à nous contacter.</p>
+
+                                                    <p>Merci de votre confiance, à bientôt.</p>
+                                                    <p>L'équipe HelloArtisan</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div class='form-row row'>
